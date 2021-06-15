7fd9179dcb93ace1b81e3291312f54ba434612a1
Spectrum Project <noob.online@yandex.ru>
2021-06-16 01:26:36 +0400

    Fixed imports and dependencies

--- src/main/java/com/testapp/wikiclient/feign/WikiClient.java
+++ src/main/java/com/testapp/wikiclient/feign/WikiClient.java
@@ -4,7 +4,11 @@ import com.testapp.wikiclient.dto.AddPageRequestDto;
 import com.testapp.wikiclient.model.PageObject;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.Map;
 
